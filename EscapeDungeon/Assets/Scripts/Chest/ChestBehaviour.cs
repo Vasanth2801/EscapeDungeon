bf604c3365e28d48f04bdcdc1d461ea0257eec82
Vasanth2801 <vasanthravichandran28@gmail.com>
2025-10-18 13:26:02 +0530

    Chest Animations

--- EscapeDungeon/Assets/Scripts/Chest/ChestBehaviour.cs
+++ EscapeDungeon/Assets/Scripts/Chest/ChestBehaviour.cs
@@ -1,14 +1,26 @@
+using System.Collections;
 using UnityEngine;
 
 public class ChestBehaviour : MonoBehaviour
 {
+    public Animator animator;
+    public float duration = 1.5f;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
-            ScoreManager.instance.AddScore(50);
-            Debug.Log("Chest Picked up");
-            Destroy(gameObject);
+            StartCoroutine(PickUp());
         }
+    }
+
+
+    IEnumerator PickUp()
+    {
+        animator.SetBool("isOpen", true);
+        ScoreManager.instance.AddScore(50);
+        Debug.Log("Chest Picked up");
+        yield return new WaitForSeconds(duration);
+        Destroy(gameObject);
     }
 }
